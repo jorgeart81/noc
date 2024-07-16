6b7198b7655ec31b206eb89817dbb892c1ed76bd
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-16 13:14:17 -0500

    feat: Create cron-service.ts

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,12 @@
+import { CheckService } from '../domain/use-cases';
+import { CronService } from './cron/cron-service';
 
 export class Server {
   static start() {
     console.log('Server started...');
+
+    CronService.createJob('*/5 * * * * *', () => {
+      new CheckService().execute('https://google.com')
+    });
   }
 }
