1019d7a36291abac9af7e887f3f78d0e032d3c82
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-17 16:01:06 -0500

    Add envs and create docker-compose

--- src/config/plugins/envs-plugin.ts
+++ src/config/plugins/envs-plugin.ts
@@ -1,9 +1,16 @@
 import * as env from 'env-var';
 
 export const envs = {
+  API_NAME: env.get('API_NAME').asString(),
   PORT: env.get('PORT').required().asPortNumber(),
-  MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
-  MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
-  MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
   PROD: env.get('PROD').required().asBool(),
+
+  MAIL_MAILER: env.get('MAIL_MAILER').asString(),
+  MAIL_HOST: env.get('MAIL_HOST').required().asString(),
+  MAIL_PORT: env.get('MAIL_PORT').required().asPortNumber(),
+  MAIL_USERNAME: env.get('MAIL_USERNAME').required().asString(),
+  MAIL_PASSWORD: env.get('MAIL_PASSWORD').required().asString(),
+  MAIL_ENCRYPTION: env.get('MAIL_ENCRYPTION').asString(),
+  MAIL_FROM_ADDRESS: env.get('MAIL_FROM_ADDRESS').asString(),
+  MAIL_FROM_NAME: env.get('MAIL_FROM_NAME').asString(),
 };
